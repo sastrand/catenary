b005f0b93bff45281d0709af381d37e69d06f5c1
Sascha Strand <sastrand@pdx.edu>
2017-11-16 08:28:38 -0800

    added json and id generator

--- client.py
+++ client.py
@@ -1,11 +1,27 @@
 import socket
+import random
+import string
+import json
+
+ID_LENGTH = 6
+
+def id_generator():
+	return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(ID_LENGTH))
 
 s = socket.socket()
-port = 12356
+port = 8080
 
-#host = socket.gethostname()
+client_id = id_generator()
+
+msg = {}
+msg['to'] = "server_initial"
+msg['from'] = client_id
+msg['body'] = "hello from client {}.".format(client_id)
+
+
+host = socket.gethostname()
 
 s.connect(('localhost', port))
 print(s.recv(1024).decode('utf-8'))
-s.send(b'\tHello from the client.')
+s.send(json.dumps(msg).encode())
 s.close
--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 # Copyright (c) 2017 Sascha Strand
 # Available open source through the MIT License
 
-# Notes from: http://www.geeksforgeeks.org/socket-programming-python/
-
 import socket
+import json
 
 s = socket.socket()
-port = 12356
+port = 8080
 
 # No address specified so any device could connect
 s.bind(('', port))
@@ -20,5 +19,6 @@ while True:
     c, addr = s.accept()
     print("\tGot connection from {}".format(addr))
     c.send(b'\tHello from the server.')
-    print(c.recv(1024).decode('utf-8'))
+    msg = json.loads(c.recv(4096).decode())
+    print("\t" + msg['body'])
     c.close()
