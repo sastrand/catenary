db6f787f3b54973ef6742004b1a9b353faf381c6
Sascha Strand <sastrand@pdx.edu>
2017-11-18 14:52:40 -0800

    multiple users, one channel

--- client.py
+++ client.py
@@ -2,32 +2,66 @@ import socket
 import random
 import string
 import json
+import sys
+import select
 
 ID_LENGTH = 6
+SERVER_HOSTNAME = 'localhost'
+PORT = 8080
+SELF_ID = "[You] "
 
 def id_generator():
 	return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(ID_LENGTH))
 
-s = socket.socket()
-port = 8080
+def prompt_flush(ps1):
+	sys.stdout.write("[" + ps1 + "] ")
+	sys.stdout.flush()
 
-client_id = id_generator()
+#-----------------------------------------------#
+#               Set up connection               #
+#-----------------------------------------------#
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+user_id = id_generator()
 
 msg = {}
 msg['to'] = "server_initial"
-msg['from'] = client_id
-msg['body'] = "hello from client {}.".format(client_id)
+msg['from'] = user_id
+msg['body'] = "hello from user {}.".format(user_id)
 
+try:
+	s.connect((SERVER_HOSTNAME, PORT))
+except:
+	print("There was a problem connecting. The server may not be running.")
+	exit()
 
-host = socket.gethostname()
-s.connect(('localhost', port))
-print(s.recv(1024).decode('utf-8'))
+sys.stdout.write("Welcome\n")
+prompt_flush(user_id)
 
-while 1:
-	msg['to'] = "server"
-	msg['from'] = client_id
-	msg['body'] = input("{}: ".format(client_id))
-	# if msg == 'Q' or msg == 'q':
-	# 	break
-	s.send(json.dumps(msg).encode())
-s.close
+#-----------------------------------------------#
+#                  Run client                   #
+#-----------------------------------------------#
+
+while True:
+	all_sockets = [sys.stdin, s]
+
+	active_sockets,_,_ = select.select(all_sockets, [], [])
+
+	for active_socket in active_sockets:
+		if active_socket == s:
+		# messages from server
+			data = active_socket.recv(4096).decode()
+			if data:
+				sys.stdout.write(data)
+				prompt_flush(user_id)
+			else:
+				print("The server has closed your connection.")
+				exit()
+		else:
+		# message to the server
+			msg['to'] = "server"
+			msg['from'] = user_id
+			msg['body'] = sys.stdin.readline()
+			s.send(json.dumps(msg).encode())
+			prompt_flush(user_id)
--- server.py
+++ server.py
@@ -3,24 +3,59 @@
 
 import socket
 import json
+import select
 
-s = socket.socket()
+WELCOME_PORT = 8080
+MAX_MSG_SIZE = 4096 #bytes 
+
+def broadcast (body, recipients, ommitted):
+	for socket in recipients:
+		if socket not in ommitted:
+			try:
+				socket.send(body.encode())
+			except Exception as e:
+				socket.close()
+				recipients.remove(socket)
+				print("A client has been disconnected due to an error: {}".format(e))
+
+#-----------------------------------------------#
+#                Set up server                  #
+#-----------------------------------------------#
+
+all_sockets = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-port = 8080
 
-# No address specified so any device could connect
-s.bind(('', port))
+# No address specified so device with an IP address can connect
+s.bind(('', WELCOME_PORT))
 
 # max of five clients at a time
 s.listen(5)
-print("Socket listening at port {}.".format(port))
+print("Server listening at port {}.".format(WELCOME_PORT))
 
-c, addr = s.accept()
-print("\tGot connection from {}".format(addr))
-c.send(b'\tHello from the server.')
+all_sockets.append(s)
 
-# The server sends and receives messages on a new socket for each connection
+#-----------------------------------------------#
+#                  Run server                   #
+#-----------------------------------------------#
+
 while True:
-    msg = json.loads(c.recv(4096).decode())
-    print("\t" + msg['body'])
-c.close()
+	active_sockets,_,_ = select.select(all_sockets, [], [])
+	for active_socket in active_sockets:
+		if active_socket == s:
+			# New connection
+			c, addr = s.accept()
+			all_sockets.append(c)
+			print("\tGot connection from {}".format(addr))
+		else:
+			try:
+				# Incoming message
+				msg = json.loads(active_socket.recv(MAX_MSG_SIZE).decode())
+				body = "\r[" + msg['from'] + "] " + msg['body']
+				broadcast(body, all_sockets, [s, active_socket])
+			except Exception as e:
+				# broadcast("User {} has left the channel\n".format(addr), all_sockets, [s, active_socket])
+				print("exception: {}".format(e))
+
+s.close()
